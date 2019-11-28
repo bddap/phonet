9bb5bd58fdd4a0d8e79fdb538a1ef712dc0510b5
Andrew Dirksen <andrew@dirksen.com>
2019-11-28 14:29:44 -0800

    just saving progress

--- trainphonet/src/main.rs
+++ trainphonet/src/main.rs
@@ -4,11 +4,20 @@ mod fft;
 mod ml_model;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = std::env::args();
+    let _ = args.next();
+    let passes: usize = args.next().unwrap_or("100".into()).parse().unwrap();
+    dbg!(passes);
+    let hidden_width = args.next().unwrap_or("10".into()).parse().unwrap();
+    dbg!(hidden_width);
+    let hidden_height = args.next().unwrap_or("256".into()).parse().unwrap();
+    dbg!(hidden_height);
+
     let training_data = audio::load_training_data("../ipa")?;
     println!("{}", training_data.close_front_unrounded_vowel.len());
     println!("{}", training_data.open_back_rounded_vowel.len());
 
-    ml_model::train(&training_data);
+    ml_model::train(&training_data, passes, (hidden_width, hidden_height));
 
     Ok(())
 }
--- trainphonet/src/ml_model.rs
+++ trainphonet/src/ml_model.rs
@@ -1,92 +1,57 @@
 use super::audio::TrainingData;
-use std::error::Error;
 use tensorflow::{
-    ops, train::AdadeltaOptimizer, train::MinimizeOptions, train::Optimizer, DataType, Output,
-    Scope, Session, SessionOptions, SessionRunArgs, Shape, Status, Tensor, Variable,
+    ops, train::AdadeltaOptimizer, train::MinimizeOptions, train::Optimizer, DataType, Operation,
+    Output, Scope, Session, SessionOptions, SessionRunArgs, Shape, Tensor, Variable,
 };
 
-// Helper for building a layer.
-//
-// `activation` is a function which takes a tensor and applies an activation
-// function such as tanh.
-//
-// Returns variables created and the layer output.
-fn layer<O1: Into<Output>>(
-    input: O1,
-    input_size: u64,
-    output_size: u64,
-    activation: &dyn Fn(Output, &mut Scope) -> Output,
-    scope: &mut Scope,
-) -> Result<(Vec<Variable>, Output), Status> {
-    let mut scope = scope.new_sub_scope("layer");
-    let scope = &mut scope;
-    let w_shape = ops::constant(&[input_size as i64, output_size as i64][..], scope)?;
-    let w = Variable::builder()
-        .initial_value(ops::random_normal(w_shape, scope)?)
-        .data_type(DataType::Float)
-        .shape(Shape::from(&[input_size, output_size][..]))
-        .build(&mut scope.with_op_name("w"))?;
-    let b = Variable::builder()
-        .const_initial_value(Tensor::<f32>::new(&[output_size]))
-        .build(&mut scope.with_op_name("b"))?;
-    Ok((
-        vec![w.clone(), b.clone()],
-        activation(
-            ops::add(
-                ops::mat_mul(input, w.output().clone(), scope)?,
-                b.output().clone(),
-                scope,
-            )?
-            .into(),
-            scope,
-        ),
-    ))
-}
+// /// A model can classify fft samples.
+// pub struct Model {
+//     session: Session,
+//     input_layer_placeholder: Operation,
+//     output_layer: Output,
+//     variables: Vec<Variable>,
+// }
 
-pub fn train(training_data: &TrainingData) {
-    let input_size = super::common::FFT_BINS as u64;
-    let hidden_size = super::common::FFT_BINS as u64;
-    let output_size = 2;
+// impl Model {
+//     pub fn create_initial(hidden_size: (u64, u64)) -> Self {
+//         let input_height = super::common::FFT_BINS as u64;
+//         let output_height = 2;
+//         let (variables, mut scope, input, output) = model(input_height, hidden_size, output_height);
 
-    let mut scope = Scope::new_root_scope();
-    let input = ops::Placeholder::new()
-        .data_type(DataType::Float)
-        .shape(Shape::from(&[1u64, input_size][..]))
-        .build(&mut scope.with_op_name("input"))
-        .unwrap();
+//         let session = Session::new(&SessionOptions::new(), &scope.graph()).unwrap();
+
+//         // Create random initial values for variables
+//         {
+//             let mut initialize = SessionRunArgs::new();
+//             for var in &variables {
+//                 initialize.add_target(&var.initializer());
+//             }
+//             session.run(&mut initialize).unwrap();
+//         }
+
+//         Model {
+//             session,
+//             input_layer_placeholder: input,
+//             output_layer: output,
+//             variables,
+//         }
+//     }
+// }
+
+pub fn train(training_data: &TrainingData, passes: usize, hidden_size: (u64, u64)) {
+    let input_height = super::common::FFT_BINS as u64;
+    let output_height = 2;
+
+    let (variables, mut scope, input, output) = model(input_height, hidden_size, output_height);
     let label = ops::Placeholder::new()
         .data_type(DataType::Float)
-        .shape(Shape::from(&[1u64, output_size][..]))
+        .shape(Shape::from(&[1u64, output_height][..]))
         .build(&mut scope.with_op_name("label"))
         .unwrap();
 
-    // Hidden layer.
-    let (vars1, layer1) = layer(
-        input.clone(),
-        input_size,
-        hidden_size,
-        &|x, scope| ops::tanh(x, scope).unwrap().into(),
-        &mut scope,
-    )
-    .unwrap();
-
-    let (vars2, output) = layer(
-        layer1.clone(),
-        hidden_size,
-        output_size,
-        &|x, _| x,
-        &mut scope,
-    )
-    .unwrap();
-
     let error = ops::subtract(output.clone(), label.clone(), &mut scope).unwrap();
     let error_squared = ops::multiply(error.clone(), error, &mut scope).unwrap();
-    let mut optimizer = AdadeltaOptimizer::new();
-    optimizer.set_learning_rate(ops::constant(1.0f32, &mut scope).unwrap());
-    let mut variables = Vec::new();
-    variables.extend(vars1);
-    variables.extend(vars2);
-    let (minimizer_vars, minimize) = optimizer
+    let (minimizer_vars, minimize) = AdadeltaOptimizer::new()
         .minimize(
             &mut scope,
             error_squared.clone().into(),
@@ -98,7 +63,7 @@ pub fn train(training_data: &TrainingData) {
     // Initialize the variables.
     // =========================
     let options = SessionOptions::new();
-    let g = scope.graph_mut();
+    let g = scope.graph();
     let session = Session::new(&options, &g).unwrap();
     let mut run_args = SessionRunArgs::new();
     // Initialize variables we defined.
@@ -114,48 +79,134 @@ pub fn train(training_data: &TrainingData) {
     // ================
     // Train the model.
     // ================
-    let mut input_tensor = Tensor::<f32>::new(&[1, input_size]);
-    let mut label_tensor = Tensor::<f32>::new(&[1, output_size]);
+    let mut input_tensor = Tensor::<f32>::new(&[1, input_height]);
+    let mut label_tensor = Tensor::<f32>::new(&[1, output_height]);
 
-    // label for "i"
-    label_tensor[0] = 1.0;
-    label_tensor[1] = 0.0;
-    for fft in &training_data.close_front_unrounded_vowel {
-        input_tensor.copy_from_slice(&fft.0);
-        let mut run_args = SessionRunArgs::new();
-        run_args.add_target(&minimize);
-        let error_squared_fetch = run_args.request_fetch(&error_squared, 0);
-        run_args.add_feed(&input, 0, &input_tensor);
-        run_args.add_feed(&label, 0, &label_tensor);
-        session.run(&mut run_args).unwrap();
-    }
+    for i in 0..passes {
+        let count = (training_data.close_front_unrounded_vowel.len()
+            + training_data.open_back_rounded_vowel.len()) as f32;
+        let mut tote = 0.0f32;
 
-    // label for "ɒ"
-    label_tensor[0] = 0.0;
-    label_tensor[1] = 1.0;
-    for fft in &training_data.open_back_rounded_vowel {
-        input_tensor.copy_from_slice(&fft.0);
-        let mut run_args = SessionRunArgs::new();
-        run_args.add_target(&minimize);
-        let error_squared_fetch = run_args.request_fetch(&error_squared, 0);
-        run_args.add_feed(&input, 0, &input_tensor);
-        run_args.add_feed(&label, 0, &label_tensor);
-        session.run(&mut run_args).unwrap();
-    }
+        // label for "i"
+        label_tensor[0] = 1.0;
+        label_tensor[1] = 0.0;
+        for fft in &training_data.close_front_unrounded_vowel {
+            input_tensor.copy_from_slice(&fft.0);
+            let mut run_args = SessionRunArgs::new();
+            run_args.add_target(&minimize);
+            let error_squared_fetch = run_args.request_fetch(&error_squared, 0);
+            run_args.add_feed(&input, 0, &input_tensor);
+            run_args.add_feed(&label, 0, &label_tensor);
+            session.run(&mut run_args).unwrap();
+            tote += run_args.fetch::<f32>(error_squared_fetch).unwrap()[0].powf(0.5);
+        }
 
-    {
-        input_tensor.copy_from_slice(
-            &training_data.open_back_rounded_vowel[training_data.open_back_rounded_vowel.len() / 2]
-                .0,
-        );
+        // label for "ɒ"
+        label_tensor[0] = 0.0;
+        label_tensor[1] = 1.0;
+        for fft in &training_data.open_back_rounded_vowel {
+            input_tensor.copy_from_slice(&fft.0);
+            let mut run_args = SessionRunArgs::new();
+            run_args.add_target(&minimize);
+            let error_squared_fetch = run_args.request_fetch(&error_squared, 0);
+            run_args.add_feed(&input, 0, &input_tensor);
+            run_args.add_feed(&label, 0, &label_tensor);
+            session.run(&mut run_args).unwrap();
+            tote += run_args.fetch::<f32>(error_squared_fetch).unwrap()[0].powf(0.5);
+        }
 
-        // let mut args = SessionRunArgs::new();
-        // args.add_feed(&input, 0, &input_tensor);
-        // args.add_feed(&label, 0, &label_tensor);
-        // let result_token = args.request_fetch(&label, 0);
-        // session.run(&mut args).unwrap();
-        // // let result_tensor = args.fetch::<f32>(result_token).unwrap();
+        if i % 100 == 0 || i == passes - 1 {
+            // evaluate training progress
+            let ave_err = tote / count;
+            dbg!(ave_err);
+        }
     }
 
     println!("done");
+}
+
+// Helper for building a layer.
+//
+// `activation` is a function which takes a tensor and applies an activation
+// function such as tanh.
+//
+// Returns variables created and the layer output.
+fn layer<O1: Into<Output>>(
+    input: O1,
+    input_size: u64,
+    output_size: u64,
+    activation: &dyn Fn(Output, &mut Scope) -> Output,
+    scope: &mut Scope,
+) -> (Vec<Variable>, Output) {
+    let mut scope = scope.new_sub_scope("layer");
+    let scope = &mut scope;
+    let w_shape = ops::constant(&[input_size as i64, output_size as i64][..], scope).unwrap();
+    let w = Variable::builder()
+        .initial_value(ops::random_normal(w_shape, scope).unwrap())
+        .data_type(DataType::Float)
+        .shape(Shape::from(&[input_size, output_size][..]))
+        .build(&mut scope.with_op_name("w"))
+        .unwrap();
+    let b = Variable::builder()
+        .const_initial_value(Tensor::<f32>::new(&[output_size]))
+        .build(&mut scope.with_op_name("b"))
+        .unwrap();
+    (
+        vec![w.clone(), b.clone()],
+        activation(
+            ops::add(
+                ops::mat_mul(input, w.output().clone(), scope).unwrap(),
+                b.output().clone(),
+                scope,
+            )
+            .unwrap()
+            .into(),
+            scope,
+        ),
+    )
+}
+
+/// Generate an untrained nn for use in phoneme classification.
+fn model(
+    input_height: u64,
+    hidden_size: (u64, u64),
+    output_height: u64,
+) -> (Vec<Variable>, Scope, Operation, Output) {
+    let mut scope = Scope::new_root_scope();
+    let mut variables = Vec::new();
+    let (hidden_width, hidden_height) = hidden_size;
+
+    let input = ops::Placeholder::new()
+        .data_type(DataType::Float)
+        .shape(Shape::from(&[1u64, input_height][..]))
+        .build(&mut scope.with_op_name("input"))
+        .unwrap();
+
+    let mut latest_layer: Output = input.clone().into();
+    let mut latest_height = input_height;
+
+    // hidden layers
+    for _ in 0..hidden_width {
+        let (mut vars, next_layer) = layer(
+            latest_layer,
+            latest_height,
+            hidden_height,
+            &|x, scope| ops::tanh(x, scope).unwrap().into(),
+            &mut scope,
+        );
+        variables.append(&mut vars);
+        latest_layer = next_layer;
+        latest_height = hidden_height;
+    }
+
+    let (mut vars, output) = layer(
+        latest_layer,
+        hidden_height,
+        output_height,
+        &|x, _| x,
+        &mut scope,
+    );
+    variables.append(&mut vars);
+
+    (variables, scope, input, output)
 }
